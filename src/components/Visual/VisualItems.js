e1d7fb1bfd98e8f9ece60d0464d99ea4db3c9200
Max Maher <maxwmaher@gmail.com>
2019-09-11 16:56:18 -0500

    updated style of visual page

--- src/components/Visual/VisualItems.js
+++ src/components/Visual/VisualItems.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from 'react-router-dom';
 import { Grid, Typography } from "@material-ui/core";
+import { Edit, Undo } from '@material-ui/icons';
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -20,10 +21,10 @@ class VisualItems extends Component {
         return (
             <Grid item sm={4} align="center">
                 
-                <Card>
+                <Card style={{ backgroundColor: "#494A49" }}>
                     <CardContent>
                         <CardActionArea>
-                            <ExpansionPanel>
+                            <ExpansionPanel style={{ backgroundColor: "#777877" }}>
                                 <ExpansionPanelSummary
                                     expandIcon={<ExpandMoreIcon />}
                                     aria-controls="panel1a-content"
@@ -47,29 +48,9 @@ class VisualItems extends Component {
                         image={this.props.visual.url}
                         title={this.props.visual.comment}
                     />
-                    {/* <CardContent>
-                        <CardActionArea>
-                            <ExpansionPanel>
-                                <ExpansionPanelSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header">
-                                    <Typography>
-                                        <b>Visual #{this.props.visual.image_number}</b>
-                                    </Typography>
-                                </ExpansionPanelSummary>
-                                <ExpansionPanelDetails>
-                                    <Typography>
-                                        <p>{this.props.visual.match_level}</p>
-                                        <p>{this.props.visual.comment}</p>
-                                    </Typography>
-                                </ExpansionPanelDetails>
-                            </ExpansionPanel>
-                        </CardActionArea>
-                    </CardContent> */}
                     <CardActions>
                         <Button>
-                            Edit
+                            <Edit style={{ marginRight: 3 }} />Edit
                     </Button>
                     </CardActions>
                 </Card>
