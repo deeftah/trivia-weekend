76bfe7d7f77354ddf64b476793038dd346474e55
maxwmaher <maxwmaher@gmail.com>
2019-11-11 09:24:35 -0600

    set defaults for demo in new contest form

--- src/components/Profile/NewContestForm.js
+++ src/components/Profile/NewContestForm.js
@@ -146,6 +146,21 @@ class NewContestForm extends Component {
         })
     };
 
+    //DEMO PURPOSES
+    handleDemo = () => {
+        console.log('handle demo hitting');
+        this.setState({
+            newContest: {
+                ...this.state.newContest,
+                contestName: 'Trivia Contest 2020',
+                startDate: '2020-02-14',
+                startTime: 17,
+                numberOfHours: 50,
+                numberOfQuestions: 9,
+            }
+        })
+    }
+
     render() {
 
         const { classes } = this.props
@@ -177,7 +192,7 @@ class NewContestForm extends Component {
         return (
 
             <div>
-                <Button variant="contained" style={{backgroundColor: this.props.user.color}} onClick={this.handleOpen}>
+                <Button variant="contained" style={{ backgroundColor: this.props.user.color }} onClick={this.handleOpen}>
                     New Contest
                 </Button>
                 <Modal
@@ -194,7 +209,7 @@ class NewContestForm extends Component {
                 >
                     <CardContent className={classes.form}>
 
-                        <h1 className={classes.h1} style={{ color: this.props.user.color}}>Enter Contest Details</h1>
+                        <h1 className={classes.h1} style={{ color: this.props.user.color }} onClick={() => this.handleDemo()}>Enter Contest Details</h1>
                         <form onSubmit={this.addContest}>
                             <div>
                                 <TextField
@@ -328,14 +343,14 @@ class NewContestForm extends Component {
                                         shrink: true
                                     }}
                                 />
-                                </div>
-                                <div>
+                            </div>
+                            <div>
                                 <Button
                                     variant="contained"
                                     type="submit"
                                     name="submit"
                                     className="contestAddButton"
-                                    style={{ marginTop: 10, backgroundColor: this.props.user.color}}>
+                                    style={{ marginTop: 10, backgroundColor: this.props.user.color }}>
                                     <AddCircle style={{ marginRight: 3 }} />Add Contest
                          </Button>
                             </div>
